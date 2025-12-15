ead31de876be0786f208f8d20d6058f93b36a663
vitasok797 <vitasok@gmail.com>
2025-12-15 22:42:25 +0300

    Modify fetch_all.py to for_each_dir.py

--- for_each_dir/for_each_dir.py
+++ for_each_dir/for_each_dir.py
@@ -3,18 +3,21 @@ import sys
 from pathlib import Path
 
 
-def fetch_dir(target_dir: Path) -> None:
-    print(f'Fetching "{target_dir.name}"... ', end='', flush=True)
+def process_dir(target_dir: Path, commands: list[str]) -> None:
+    print(f'Processing dir "{target_dir.name}"... ', end='', flush=True)
 
     if not target_dir.is_dir():
         print('Error (target dir not found)')
         sys.exit(1)
 
-    args = ['git', 'fetch', '--all']
+    for command in commands:
+        process_command(target_dir, command)
 
+
+def process_command(target_dir: Path, command: str) -> None:
     try:
         subprocess.run(
-                args,
+                command,
                 cwd=target_dir,
                 capture_output=True,
                 shell=True,
@@ -27,12 +30,14 @@ def fetch_dir(target_dir: Path) -> None:
 
 
 def main() -> None:
+    commands = sys.argv[1:]
+
     work_dir = Path()
     dir_items = work_dir.glob('*')
     dirs = [item for item in dir_items if item.is_dir()]
 
     for target_dir in dirs:
-        fetch_dir(target_dir)
+        process_dir(target_dir, commands)
 
     print('Done')
 
